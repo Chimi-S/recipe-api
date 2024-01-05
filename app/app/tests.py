79446ceed09d8bb916b8331d844984c280a8de09
Chimi-S <chimithinley705@gmail.com>
2024-01-06 00:15:04 +0600

    Added the test file

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""Sample Test"""
+
+from django.test import SimpleTestCase
+
+from app import calculator
+
+class CalculateTest(SimpleTestCase):
+    
+    def test_add_numbers(self):
+
+        res = calculator.add(5,6)
+
+        self.assertEqual(res,11)
