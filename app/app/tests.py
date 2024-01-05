717adf4a01c9f156133521977e9ecb04f6b13c45
Chimi-S <chimithinley705@gmail.com>
2024-01-06 00:38:01 +0600

    Added Test File

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,22 @@
 """
-Sample Test
+Sample tests
 """
 from django.test import SimpleTestCase
 
 from app import calculator
 
 
-class CalculateTest(SimpleTestCase):
-    
+class CalcTests(SimpleTestCase):
+    """Test the calculator module."""
 
     def test_add_numbers(self):
-
+        """Test adding numbers together."""
         res = calculator.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calculator.subtract(10, 15)
+
+        self.assertEqual(res, 5)
